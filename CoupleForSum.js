63e161c3b34a97de535fadaceb1fbbc0ee3ce534
Scherbakov Mike <scherbakov.mike@gmail.com>
2023-07-25 19:36:09 +0300

    The task "Two sum" has been solved.

--- CoupleForSum.js
+++ CoupleForSum.js
@@ -17,15 +17,32 @@ function coupleForSum_naive(array, sum) {
 function coupleForSum_complements(array, sum) {
     let result = false;
     const complements = new Set()
-    for(const element of array) {
+    for (const element of array) {
         const complement = sum - element;
-        if(complements.has(complement)) {
+        if (complements.has(complement)) {
             return true;
         }
         complements.add(element);
     }
     return result;
 }
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    const complements = new Map()
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (complements.has(complement)) {
+            return [i, complements.get(complement)];
+        }
+        complements.set(nums[i], i);
+    }
+    return [];
+};
 
 const array = Array.from(Array(10000).keys());
 console.time("Naive")
@@ -34,3 +51,5 @@ console.timeEnd("Naive")
 console.time("Complements")
 console.log(coupleForSum_complements(array, 19997));
 console.timeEnd("Complements")
+
+console.log(twoSum([2,7,11,15], 9))
