303ff98e24e4b6e1f6f5192d35832cd18f68b9a0
Scherbakov Mike <scherbakov.mike@gmail.com>
2023-07-27 19:35:11 +0300

    Optimized solution of the task "Maximum sub array" has been implemented using Kadane's algorithm.

--- MaximumSubArray.java
+++ MaximumSubArray.java
@@ -18,8 +18,26 @@ public class MaximumSubArray {
         return max;
     }
 
+    public static int maxSubArrayLinear(int[] nums) {
+        var result = Integer.MIN_VALUE;
+        var curSum = 0;
+        for (var n : nums) {
+            curSum = curSum + n;
+            if (curSum > result)
+                result = curSum;
+            if (curSum < 0)
+                curSum = 0;
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
-        var nums = new int[]{5, 4, -1, 7, 8};
+        //var nums = new int[]{5, 4, -1, 7, 8};
+        var nums = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        //var nums = new int[]{-2, 1};
+        //var nums = new int[]{-2, -1};
+        //var nums = new int[]{-1, -2};
         System.out.println(maxSubArrayNaive(nums));
+        System.out.println(maxSubArrayLinear(nums));
     }
 }
